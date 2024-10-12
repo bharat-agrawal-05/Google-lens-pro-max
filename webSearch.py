636faad5643ffc83f5ee85be01cff3da59c85de6
Bharat Agrawal <141904232+bharat-agrawal-05@users.noreply.github.com>
2024-10-12 16:50:49 +0530

    git ignore added and websearch modified

--- webSearch.py
+++ webSearch.py
@@ -1,22 +1,22 @@
 import pandas as pd
 from duckduckgo_search import DDGS
 
-# Define the search query
-search_query = 'Ratan Tata'
+def search(keywords):
+    search_query = keywords
 
-# Fetch search results from DuckDuckGo
-results = DDGS().text(
-    keywords=search_query,
-    region='wt-wt',
-    safesearch='off',
-    max_results=50  # Limit the number of results
-)
+    # Fetch search results from DuckDuckGo
+    results = DDGS().text(
+        keywords=search_query,
+        region='wt-wt',
+        safesearch='off',
+        max_results=20  # Limit the number of results
+    )
 
-# Print the results for verification
-print(results)
+    # Print the results for verification
+    print(results)
 
-# Create a DataFrame from the results
-results_df = pd.DataFrame(results)
+    # Create a DataFrame from the results
+    results_df = pd.DataFrame(results)
 
-# Save the DataFrame to a CSV file
-results_df.to_csv('duckduck_tutorial.csv', index=False)
+    # Save the DataFrame to a CSV file
+    results_df.to_csv('duckduck_tutorial.csv', index=False)
