361bd4adaa3c11fbfea37f93031440d4825dc337
Bharat Agrawal <141904232+bharat-agrawal-05@users.noreply.github.com>
2024-10-14 23:54:41 +0530

    done, only readme remains

--- README.md
+++
@@ -1,8 +0,0 @@
-# Google-lens-pro-max
-
-### Installation and Requirements
-You have to install Ollama to use the LLaVA model, this model gives more accurate answers.
-
-OR
-
-You can use BLIP model
---
+++ WebPage/backend/llava.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template
+from PIL import Image
+import os
+import sys
+from transformers import BlipProcessor, BlipForConditionalGeneration
+import pandas as pd
+from duckduckgo_search import DDGS
+import ollama
+
+def image_content(image_path):
+    res = ollama.chat(
+        model = 'llava',
+        messages = [
+            {
+                'role': 'user',
+                'content': 'describe the image in one sentence',
+                'image': image_path
+            }
+        ]
+    )
+    print(res['message']['content'])
+    return res['message']['content']
+
+def search(keywords):
+    search_query = keywords
+
+    # Fetch search results from DuckDuckGo
+    results = DDGS().text(
+        keywords=search_query,
+        region='wt-wt',
+        safesearch='off',
+        max_results=20
+    )
+
+    # Convert results to DataFrame and save to CSV
+    results_df = pd.DataFrame(results)
+    results_df.to_csv('output.csv', index=True)
+
+    return results
+
+image_path = sys.argv[1]
+prompt = sys.argv[2]
+
+# Generate caption
+caption = image_content(image_path)
+caption = f"I have a image with content:{caption} and I want to search for {prompt}"
+with open('caption.txt', 'w') as f:
+    f.write(caption)
+search(caption)
+
